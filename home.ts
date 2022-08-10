37fd142186bb70a98e82c32dcbf03927648d1408
OGUGUA54 <nndukaobikwelu@gmail.com>
2022-08-10 11:02:46 +0100

    new commit

---
+++ home.ts
@@ -0,0 +1,7 @@
+const firstName: string = 'john';    
+let age: number = 30;
+let lastname: string = 'Doe';
+console.warn('last name variable is in camel case', lastname);
+if (typeof(age) !== 'number') {
+    console.error('Age is not a number')
+}
